78266875ad8da887846ba638227647bd3d11860b
Samuel Belarmino <johnbelarmino1@yahoo.com>
2019-07-23 17:57:29 -0700

    Changes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
     return (
       <Context>
         <Router className="App">
-          <Route exact path="/" component={Home} />
+          <Route exact path="/bnb-advisor/" component={Home} />
           <Route exact path="/favorites" component={Favorites} />
           <Route exact path="/results" component={ResultsPage} />
         </Router>
